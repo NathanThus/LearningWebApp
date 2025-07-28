8d01ed61f88aa9b31b925f98f30a0405ac66002f
NathanThus <nathan.thus@outlook.com>
2025-07-28 13:00:24 +0200

    ✨ Implement new Adafruit AHT20 Library and timing
    Now runs once an hour.

--- WeatherSensor/src/main.cpp
+++ WeatherSensor/src/main.cpp
@@ -1,18 +1,19 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include "AHT20.h"
+#include <Adafruit_AHTX0.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
 
 #define DELAY_RECONNECTION_MILLISECONDS 100
 #define RECONNECTION_ATTEMPTS 5
+#define CONVERT_HOURS_TO_MILLISECONDS * 3600000
+#define DELAY_WEATHER_PULSE_MILLISECONDS 1 CONVERT_HOURS_TO_MILLISECONDS
 
 char SSID[] = "WiFi";
 char password[] = "PassWord";
 String dbAddress = "http://localhost:3000/weather";
 
-AHT20 aht20;
-float humidity, temperature;
+Adafruit_AHTX0 aht20;
+sensors_event_t humidity, temperature;
 HTTPClient httpClient;
 
 void setup()
@@ -25,8 +26,13 @@ void setup()
     delay(DELAY_RECONNECTION_MILLISECONDS);
     WiFi.begin();
   }
+}
 
-
+/// @brief Grabs the sensor data and allocates it to @b humidity and @b temperature.
+/// @return Returns true if data was succesfully collected. 
+bool GetSensorData()
+{
+  return aht20.getEvent(&humidity,&temperature);
 }
 
 void loop()
@@ -35,8 +41,12 @@ void loop()
   httpClient.begin(dbAddress);
 
   httpClient.addHeader("Content-Type", "application/json");
-  int temp = (int) temperature;
-  int humi = (int) humidity;
+
+  // The adafruit library does have some wierd practices, but it is what it is.
+  int temp = (int) temperature.temperature;
+  int humi = (int) humidity.relative_humidity;
+
+  // Apparently string REALLY doesn't like value injection, hence the multiline approach.
   String payload = "{\"temperature\": \"" + temp;
   payload += ", \"humidity\": \""+ humi;
   payload += "\"}";
@@ -44,11 +54,5 @@ void loop()
   httpClient.POST(payload);
 
   httpClient.end();
-}
-
-/// @brief Grabs the sensor data and allocates it to @b humidity and @b temperature.
-/// @return Returns true if data was succesfully collected. 
-bool GetSensorData()
-{
-  return aht20.getSensor(&humidity,&temperature) == 0;
+  delay(DELAY_WEATHER_PULSE_MILLISECONDS);
 }
